7f9c5b9ae39a79a69aa8597e8de7e8dcec41b380
Adnan Niaz <sanixstudio@gmail.com>
2023-01-17 19:45:15 -0800

    initial setups

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./main.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-8xl text-emerald-300">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
